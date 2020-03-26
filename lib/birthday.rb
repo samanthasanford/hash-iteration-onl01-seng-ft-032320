43ff16b00f56e3f1b41f852bab6ecb8ec9b204c0
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 23:26:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,8 +17,12 @@ def age_appropriate_birthday(birthday_kids)
   
   birthday_kids.each do |kids_name, age|
   
-  if age <= 12
+  if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  
+  elsif age == 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    
   else
     "You are too old for this"
 end

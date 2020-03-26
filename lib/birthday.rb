6db7f9865f8922034dc50cec1132560b33a1215c
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 23:27:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -21,7 +21,7 @@ def age_appropriate_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     
   else
-    "You are too old for this"
+    "You are too old for this."
 end
 end
 end
